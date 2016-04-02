6ff6c7b4f9577b35fb5d130d031071ebad6ac334
Jim Bulkowski <jim.b@paperelectron.com>
2016-04-02 14:33:38 -0400

    Adds merge plugin

---
+++ index.js
@@ -0,0 +1,39 @@
+/**
+ * @file index
+ * @author Jim Bulkowski <jim.b@paperelectron.com>
+ * @project pomegranate-sequelize-core
+ * @license MIT {@link http://opensource.org/licenses/MIT}
+ */
+
+'use strict';
+var Sequelize = require('sequelize')
+/**
+ *
+ * @module index
+ */
+
+exports.metadata = {
+  name: 'Sequelize',
+  layer: 'core',
+  param: 'SQL',
+  type: 'merge'
+}
+
+exports.plugin = {
+  load: function(inject, loaded) {
+    loaded(null, {Sequelize: Sequelize})
+  },
+  start: function(done) {
+    done()
+  },
+  stop: function(done) {
+    done()
+  }
+}
+
+exports.errors = {
+  ValidationError: Sequelize.ValidationError,
+  UniqueConstraintError: Sequelize.UniqueConstraintError,
+  ExclusionConstraintError: Sequelize.ExclusionConstraintError,
+  ForeignKeyConstraintError: Sequelize.ForeignKeyConstraintError
+}
